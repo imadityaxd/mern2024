7030be43d6fe3a61df9bd4adb498fdb51e065185
imadityaxd <104629524+imadityaxd@users.noreply.github.com>
2024-06-17 21:41:09 +0530

    17june

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -5,8 +5,21 @@ export const Register = () =>{
                 <div className = "section-registration">
                     <div className="container grid grid-two-cols">
                         <div className="registration-image">
-                            <img src ="" alt=""/>
+                            <img src ="/images/lisp.png" alt="a simple registraion image" width="400" height="400"/>
 
+                        </div>
+                        {/*let's crate registration form  */}
+                        <div>
+                            <div className= "registration-form">
+                                <h1 className="main-heading mb-3">Registration form</h1>
+                                <br/>
+                                <form>
+                                    <div>
+                                        <label htmlFor="email">email</label>
+                                        <input type="text" name="email" placeholder="email" id="email" required autoComplete="off"/>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
